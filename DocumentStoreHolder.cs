0b54c0f39c7129c406cdf1f83822e841fa0c61f1
Cina <memaralicina@gmail.com>
2019-07-16 17:28:20 -0400

    Trying to connect this Raven

---
+++ DocumentStoreHolder.cs
@@ -0,0 +1,17 @@
+﻿using Raven.Client.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KrawkowSociety
+{
+    public interface IDocumentStoreHolder
+    {
+        IDocumentStore Store { get; }
+    }
+
+    public class DocumentStoreHolder
+    {
+    }
+}
---
+++ Models/RavenSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KrawkowSociety.Models
+{
+    public class RavenSettings
+    {
+        public string Url { get; set; }
+        public string Database { get; set; }
+    }
+}
