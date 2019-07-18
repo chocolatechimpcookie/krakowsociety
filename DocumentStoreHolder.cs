9c1c947ff38672b809f36706d480abca712dc270
Cina <memaralicina@gmail.com>
2019-07-18 17:24:49 -0400

    RavenDB document is not being created. Not sure why. Its not reaching the inside of the idocumentstoreholder

--- DocumentStoreHolder.cs
+++ DocumentStoreHolder.cs
@@ -20,11 +20,11 @@ namespace KrawkowSociety
         private readonly ILogger<DocumentStoreHolder> _logger;
         public IDocumentStore Store { get; }
 
-        //private void CreateDatabaseIfNotExists
 
         public DocumentStoreHolder(IOptions<RavenSettings> ravenSettings, ILogger<DocumentStoreHolder> logger)
         {
             this._logger = logger;
+            this._logger.LogInformation("opening up document store holder");
             var settings = ravenSettings.Value;
             Store = new DocumentStore()
             {
@@ -33,7 +33,9 @@ namespace KrawkowSociety
             };
 
             Store.Initialize();
+            this._logger.LogInformation("store initializes");
             CreateDatabaseIfNotExists();
+            this._logger.LogInformation("create db");
 
 
 
@@ -41,10 +43,30 @@ namespace KrawkowSociety
         private void CreateDatabaseIfNotExists()
         {
             var database = Store.Database;
+
             // Only RavenDB.Client 4.0.0-rc-40023 not 4.0.0 has the admin variable
-            // either I need a replacement or I need the same client
             // I might need to rewrite this to keep it up to date or just keep copies of this dependency
             var dbRecord = Store.Admin.Server.Send(new GetDatabaseRecordOperation(database));
+
+            if (dbRecord == null)
+            {
+                this._logger.LogInformation("RavenDB database does not exist");
+                dbRecord = new DatabaseRecord(database);
+
+                var createResult = Store.Admin.Server.Send(new CreateDatabaseOperation(dbRecord));
+
+                if (createResult.Name != null)
+                {
+                    // can seed data here
+                    using (var session = Store.OpenSession())
+                    {
+  
+                        session.SaveChanges();
+                    }
+                }
+
+
+            }
 
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using KrawkowSociety.Models;
+using Microsoft.Extensions.Logging;
+
+using Raven.Client.Documents;
 
 namespace KrawkowSociety
 {
@@ -20,11 +23,16 @@ namespace KrawkowSociety
             Configuration = configuration;
         }
 
+        //private readonly ILogger<Startup> _logger;
+
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //this._logger = logger;
+            //this._logger.LogInformation("startupppppppppppppppppppppppppppppppp");
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -32,8 +40,13 @@ namespace KrawkowSociety
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddOptions();
             services.Configure<RavenSettings>(Configuration.GetSection("Raven"));
             services.AddSingleton<IDocumentStoreHolder, DocumentStoreHolder>();
+
+            //talk data services, these have to be written out
+            //services.AddSingleton<InMemoryTalkService, InMemoryTalkService>();
+            //services.AddTransient
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
